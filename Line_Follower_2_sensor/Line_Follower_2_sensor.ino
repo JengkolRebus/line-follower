5a54d9d27942d2a79345612b86e0674767f68fba
_jengkolrebus <64493984+JengkolRebus@users.noreply.github.com>
2021-10-18 00:23:08 +0700

    Add files via upload

---
+++ Line_Follower_2_sensor/Line_Follower_2_sensor.ino
@@ -0,0 +1,39 @@
+int s1 = 0;
+int s2 = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  
+  pinMode(3, OUTPUT);
+  pinMode(11, OUTPUT);
+  analogWrite(3, 90);
+  analogWrite(11, 80);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  s1 = digitalRead(7);
+  s2 = digitalRead(8);
+  
+  if(s1 == 0 && s2 == 0){
+    maju();
+  }
+  if(s1 == 0 && s2 == 1){
+    bKiri();
+  }
+  if(s1 == 1 && s2 == 0){
+    bKanan();
+  }
+  /*
+  if(s1 == 1 && s2 == 1){
+    Stop();
+  }
+  */
+}
---
+++ Line_Follower_2_sensor/gerakMotor.ino
@@ -0,0 +1,28 @@
+void maju(){
+  digitalWrite(5, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(9, HIGH);
+}
+
+void bKiri(){
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+}
+
+void bKanan(){
+  digitalWrite(6, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(9, HIGH);
+}
+
+void Stop(){
+  digitalWrite(5, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(9, LOW);
+}
+
